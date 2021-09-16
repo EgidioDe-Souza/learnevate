1c6108095549f71da97915b7ee47966cd3b1ec64
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-09-16 19:31:16 +0100

    Created the layout of the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,11 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
+
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false)
+
     return (
       <React.Fragment>
         <nav className="navbar">
@@ -8,7 +13,44 @@ function Navbar() {
             <Link to="/" className="navbar-logo">
               Learnevate <i className="fab fa-typo3" />
             </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            </div>
           </div>
+          <ul className={click ? "nav-menu action" : "nav-menu"} >
+            <li className="nav-item">
+              <Link to="/" 
+              className="nav-links" 
+              onClick={closeMobileMenu}
+              >
+                Home    
+              </Link>
+            </li>    
+            <li className="nav-item">
+              <Link to="/services" 
+              className="nav-links" 
+              onClick={closeMobileMenu}
+              >
+                Services     
+              </Link>
+            </li>    
+            <li className="nav-item">
+              <Link to="/contact" 
+              className="nav-links" 
+              onClick={closeMobileMenu}
+              >
+                Contact  
+              </Link>
+            </li>    
+            <li className="nav-item">
+              <Link to="/sign-up" 
+              className="nav-links-mobile" 
+              onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>    
+          </ul>
         </nav>        
       </React.Fragment>
     )
