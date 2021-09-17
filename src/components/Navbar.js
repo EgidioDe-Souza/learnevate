e5bd098d868ce091bf33d2fd7cbb1dd82c52d2e7
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-09-17 12:03:49 +0100

    implemented button component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { Button } from './Button'
 import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+      if(window.innerWidth <= 960) {
+        setButton(false);
+      } else {
+        setButton(true);
+      }
+    };
+
+    window.addEventListener("resize", showButton);
 
     return (
       <React.Fragment>
@@ -16,41 +29,43 @@ function Navbar() {
             <div className="menu-icon" onClick={handleClick}>
               <i className={click ? "fas fa-times" : "fas fa-bars"} />
             </div>
+            <ul className={click ? "nav-menu action" : "nav-menu"} >
+              <li className="nav-item">
+                <Link to="/" 
+                className="nav-links" 
+                onClick={closeMobileMenu}
+                >
+                  Home    
+                </Link>
+              </li>    
+                <li className="nav-item">
+                <Link to="/services" 
+                className="nav-links" 
+                onClick={closeMobileMenu}
+                >
+                  Services     
+                </Link>
+              </li>    
+              <li className="nav-item">
+                <Link to="/contact" 
+                className="nav-links" 
+                onClick={closeMobileMenu}
+                >
+                  Contact  
+                </Link>
+              </li>    
+              <li className="nav-item">
+                <Link to="/sign-up" 
+                className="nav-links-mobile" 
+                onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>    
+            </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</
+          Button>}  
           </div>
-          <ul className={click ? "nav-menu action" : "nav-menu"} >
-            <li className="nav-item">
-              <Link to="/" 
-              className="nav-links" 
-              onClick={closeMobileMenu}
-              >
-                Home    
-              </Link>
-            </li>    
-            <li className="nav-item">
-              <Link to="/services" 
-              className="nav-links" 
-              onClick={closeMobileMenu}
-              >
-                Services     
-              </Link>
-            </li>    
-            <li className="nav-item">
-              <Link to="/contact" 
-              className="nav-links" 
-              onClick={closeMobileMenu}
-              >
-                Contact  
-              </Link>
-            </li>    
-            <li className="nav-item">
-              <Link to="/sign-up" 
-              className="nav-links-mobile" 
-              onClick={closeMobileMenu}
-              >
-                Sign Up
-              </Link>
-            </li>    
-          </ul>
         </nav>        
       </React.Fragment>
     )
