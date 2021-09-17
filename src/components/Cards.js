910906cb6ed16db1f8cf15ce352ddb2bce3169e5
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-09-17 22:43:18 +0100

    added styling and path to cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,7 +9,42 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem />
+            <CardItem 
+             src="images/img-9.jpg"
+             text="Reading Books"
+             label="Reading"
+             path="/services"
+            />
+           
+            <CardItem 
+             src="images/img-2.jpg"
+             text="Enrol in an online course"
+             label="Studying"
+             path="/services"
+            />
+           
+          </ul>
+          <ul className="cards__items">
+            <CardItem 
+             src="images/img-9.jpg"
+             text="Reading Books"
+             label="Reading"
+             path="/services"
+            />
+           
+            <CardItem 
+             src="images/img-2.jpg"
+             text="Enrol in an online course"
+             label="Studying"
+             path="/services"
+            />
+            <CardItem 
+             src="images/img-2.jpg"
+             text="Enrol in an online course"
+             label="Studying"
+             path="/services"
+            />
+           
           </ul>
         </div>    
       </div>   
