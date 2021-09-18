ee9775b80b271e85733255f86987bcc590bb9d32
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-09-18 16:34:46 +0100

    Added paths contact, services and signup page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Contact from './components/pages/Contact';
+import SignUp from './components/pages/SignUp';
 
 
 
@@ -12,8 +15,11 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component=
-          {Home} />
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/sign-up" component={SignUp} />
+
         </Switch>
       </Router>
       
